b13ec53db45f8fa02f423f0b753599fd74c03fe6
martin <martinherrero31@gmail.com>
2018-05-21 19:30:11 -0300

    command de condiciones

--- sge/src/main/java/sge/regla/Condicion.java
+++ sge/src/main/java/sge/regla/Condicion.java
@@ -3,14 +3,20 @@ package sge.regla;
 public class Condicion {
 
 	private double valorEsperado;
-	Sensor sensor;
+	private Sensor sensor;
+	private Comparador comp;
 	
+	public Condicion(double _valorEsperado,Sensor _sensor,Comparador _comp ) {
+		valorEsperado=_valorEsperado;
+		sensor=_sensor;
+		comp=_comp;
+	}
 	public boolean verificar() {
 		return this.comparacion(valorEsperado,sensor.getMedicion());
 	}
-	private boolean comparacion(double valorEsperado2, double medicion) {
+	private boolean comparacion(double valorEsperado, double medicion) {
 		
-		return  valorEsperado==medicion;
+		return  comp.comparacion(valorEsperado, medicion);
 	}
 
 
