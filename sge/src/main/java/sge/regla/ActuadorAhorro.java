5a334f123d254a8470e255310377dd64f4407954
abelFariasJuarez <fari2207@gmail.com>
2018-05-21 19:36:50 -0300

    mas actuador

--- sge/src/main/java/sge/regla/ActuadorAhorro.java
+++ sge/src/main/java/sge/regla/ActuadorAhorro.java
@@ -5,12 +5,14 @@ import sge.dispositivo.Inteligente;
 public class ActuadorAhorro implements Actuador {
 
 	private Inteligente dispositivo;
+
 	public ActuadorAhorro(Inteligente _dispositivo) {
-		dispositivo=_dispositivo;
+		dispositivo = _dispositivo;
 	}
+
 	@Override
 	public void ejecutarAccion() {
-dispositivo.ahorroDeEnergia();
+		dispositivo.ahorroDeEnergia();
 	}
 
 }
