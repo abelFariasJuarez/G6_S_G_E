c65a948420fc2b19230fa1c84a0a5d7b8e1fb7d6
martin <martinherrero31@gmail.com>
2018-05-22 05:16:32 -0300

    agregado constructor borrado sin querer

--- sge/src/main/java/sge/Cliente.java
+++ sge/src/main/java/sge/Cliente.java
@@ -24,7 +24,16 @@ public class Cliente extends UsuarioSGE {
 		telefono = _telefono;
 
 	}
+	public Cliente(String _nombre, String _apellido, String _domicilio, LocalDate _fechaIngreso, String _username,
+			String _password, String _tipodoc, Integer _nrodoc, Integer _telefono, List<Dispositivo> _dispositivos) {
+		super(_nombre, _apellido, _domicilio, _fechaIngreso, _username, _password);
+		tipodoc = _tipodoc;
+		nrodoc = _nrodoc;
+		telefono = _telefono;
+		dispositivos=_dispositivos;
 
+	}
+	
 	public String getTipoDoc() {
 		return tipodoc;
 	}
