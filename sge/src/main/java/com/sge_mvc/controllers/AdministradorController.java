04afd455a925fd69b7fc41f3c615dc509fe655a1
abelFariasJuarez <fari2207@gmail.com>
2018-12-15 07:44:10 -0300

    clientes unicos y dispositivos disponibles table

--- sge/src/main/java/com/sge_mvc/controllers/AdministradorController.java
+++ sge/src/main/java/com/sge_mvc/controllers/AdministradorController.java
@@ -17,46 +17,65 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import sge.modelo.dispositivo.DispositivoDisponible;
 import sge.modelo.usuarios.Cliente;
 import sge.repositorios.Repositorio;
 
 @Controller
 @RequestMapping("/demo/login/Administrador")
 public class AdministradorController {
-	
-	@RequestMapping (method = RequestMethod.POST)
+
+	@RequestMapping(method = RequestMethod.POST)
 	public String x() {
 		return "Administrador";
 	}
-	@RequestMapping(value="cargadispo", method = RequestMethod.POST,params="snd")
-	public String s(@RequestParam("file") MultipartFile file, ModelMap modelMap) {	
-	    modelMap.addAttribute("file", file);
+
+	@RequestMapping(value = "cargadispo", method = RequestMethod.POST, params = "snd")
+	public String s(@RequestParam("file") MultipartFile file, ModelMap modelMap) {
+		modelMap.addAttribute("file", file);
 		Repositorio repo = new Repositorio();
 		repo.abrir();
 		String content = new String(file.getOriginalFilename());
 		repo.dispositivosDisponibles().cargarDispositivos(content);
 		repo.dispositivosDisponibles().guardarDispositivosDisponibles();
-	    return "cargadispo";
+		return "cargadispo";
 	}
-	
-	@RequestMapping(value="cargadispo", method = RequestMethod.POST ,params="oculto2")
+
+
+	/*@RequestMapping(value = "cargadispo", method = RequestMethod.POST, params = "oculto2")
 	public String submit() {
-	    return "cargadispo";
+		return "cargadispo";
 	}
+	*/
+
 	
-	@RequestMapping(value="consumo",method = RequestMethod.POST)
-	public ModelAndView consumo() {
-		List<Cliente> clientes= new ArrayList<Cliente>();
+	@RequestMapping(value = "cargadispo", method = RequestMethod.POST)
+	public ModelAndView cargadispo() {
+		List<DispositivoDisponible> disponibles = new ArrayList<DispositivoDisponible>();
 		Repositorio repo = new Repositorio();
 		repo.abrir();
-		clientes=repo.clientes().all();
-		 ModelAndView modelAndView = new ModelAndView("consumo");
-		 modelAndView.addObject("clientes", clientes);
-		 repo.cerrar();
-	     return modelAndView;
+		disponibles = repo.dispositivosDisponibles().all();
+		ModelAndView modelAndView = new ModelAndView("cargadispo");
+		modelAndView.addObject("dispositivosDisponibles", disponibles);
+		repo.cerrar();
+		return modelAndView;
 
 	}
-	@RequestMapping(method=RequestMethod.GET)
+	
+	@RequestMapping(value = "consumo", method = RequestMethod.POST)
+	public ModelAndView consumo() {
+		List<Cliente> clientes = new ArrayList<Cliente>();
+		Repositorio repo = new Repositorio();
+		repo.abrir();
+		clientes = repo.clientes().all();
+		ModelAndView modelAndView = new ModelAndView("consumo");
+		modelAndView.addObject("clientes", clientes);
+		repo.cerrar();
+		return modelAndView;
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
 	public String wse() {
 		return "Administrador";
 	}
