1b630b50ce287a817b93c86a0c3423375c680cda
martinherrero <martinherrero31@gmail.com>
2018-12-22 00:01:24 -0300

    simplex

--- sge/src/main/java/sge/repositorios/Repositorio.java
+++ sge/src/main/java/sge/repositorios/Repositorio.java
@@ -364,7 +364,7 @@ public class Repositorio {
 		if (cliente1 == null) {
 			cliente1 = new Cliente("Felipe", "Perez", "Medrano 123", LocalDate.of(1995, 4, 7), "fperez", "menToL2017",
 					"Dni", 11222333, 1543312311);
-			cliente1.setAhorroAutomatico(false);
+			cliente1.setAhorroAutomatico(true);
 			cliente1.setUbicacion(new Ubicacion(10.0, 90.0));
 			this.persistir(cliente1);
 		}
